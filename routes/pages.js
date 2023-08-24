d30bda25525c336a2de543c2217103b10f2c411c
Nafis Azizi Riza <101693218+nafisazizir@users.noreply.github.com>
2023-08-24 23:30:21 +0800

    feat: styling home page

--- routes/pages.js
+++ routes/pages.js
@@ -1,34 +1,48 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const mysql = require('mysql');
-const dotenv = require('dotenv');
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+const dotenv = require("dotenv");
 
-dotenv.config({path:'./config.env'})
+dotenv.config({ path: "./config.env" });
 
 const db = mysql.createConnection({
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT, 
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE,
-    socketPath: process.env.DATABASE_SOCKET
-})
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE,
+  socketPath: process.env.DATABASE_SOCKET,
+});
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    const query = 'SELECT * FROM anak';
-    db.query(query, (error, anakData) => {
-        if (error) {
-            console.log(error);
-            res.render('error', { message: 'Error retrieving anak data.' });
-        } else {
-            res.render('main', { anakList: anakData });
-        }
-    });
- 
-})
-
+router.get("/", (req, res) => {
+  const query = "SELECT * FROM ANAK ORDER BY TAHUN_MASUK, NAMA, TANGGAL_LAHIR";
+  db.query(query, (error, anakData) => {
+    if (error) {
+      console.log(error);
+      res.render("error", { message: "Error retrieving anak data." });
+    } else {
+      const formattedAnakData = anakData.map((anak) => {
+        const dateOfBirth = new Date(anak.tanggal_lahir);
+        const options = {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        };
+        const formattedDateOfBirth = dateOfBirth.toLocaleDateString(
+          "id-ID",
+          options
+        );
+        return {
+          ...anak,
+          tanggal_lahir: formattedDateOfBirth,
+        };
+      });
+      res.render("main", { anakList: formattedAnakData });
+    }
+  });
+});
 
 module.exports = router;
