73cab7f4050d9b6d4206c66f68f13b8383bd0f39
Nafis Azizi Riza <101693218+nafisazizir@users.noreply.github.com>
2023-08-25 11:53:58 +0800

    feat: styling details page

--- routes/pages.js
+++ routes/pages.js
@@ -47,74 +47,132 @@ router.get("/", (req, res) => {
 
 // routes/pages.js
 
-router.get('/anak/:id', (req, res) => {
-    const anakId = req.params.id;
+router.get("/anak/:id", (req, res) => {
+  const anakId = req.params.id;
 
-    // Fetch the child's details and associated rapot data from the database
-    const queryAnak = 'SELECT * FROM anak WHERE nisn = ?';
-    const queryRapot = 'SELECT * FROM rapot WHERE anak_id = ?';
+  // Fetch the child's details and associated rapot data from the database
+  const queryAnak = "SELECT * FROM anak WHERE nisn = ?";
+  const queryRapot = "SELECT * FROM rapot WHERE anak_id = ?";
 
-    db.query(queryAnak, [anakId], (error, anakData) => {
-        if (error || anakData.length === 0) {
-            console.log(error);
-            res.render('error', { message: 'Child not found.' });
+  db.query(queryAnak, [anakId], (error, anakData) => {
+    if (error || anakData.length === 0) {
+      console.log(error);
+      res.render("error", { message: "Child not found." });
+    } else {
+      db.query(queryRapot, [anakId], (rapotError, rapotData) => {
+        if (rapotError) {
+          console.log(rapotError);
+          res.render("error", { message: "Error retrieving rapot data." });
         } else {
-            db.query(queryRapot, [anakId], (rapotError, rapotData) => {
-                if (rapotError) {
-                    console.log(rapotError);
-                    res.render('error', { message: 'Error retrieving rapot data.' });
-                } else {
-                    res.render('anakDetails', { anak: anakData[0], rapotList: rapotData });
-                }
-            });
+          const dateOfBirth = new Date(anakData[0].tanggal_lahir);
+          const options = {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          };
+          const formattedDateOfBirth = dateOfBirth.toLocaleDateString(
+            "id-ID",
+            options
+          );
+          const formattedRaporData = rapotData.map((rapor) => {
+            const tanggal_tindakan = new Date(rapor.tanggal_tindakan);
+            const options = {
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            };
+            const formattedTanggalTindakan =
+              tanggal_tindakan.toLocaleDateString("id-ID", options);
+            return {
+              ...rapor,
+              tanggal_tindakan: formattedTanggalTindakan,
+            };
+          });
+          res.render("anakDetails", {
+            anak: {
+              nama: anakData[0].nama,
+              nisn: anakData[0].nisn,
+              jenis_kelamin: anakData[0].jenis_kelamin,
+              tanggal_lahir: formattedDateOfBirth,
+              tahun_masuk: anakData[0].tahun_masuk,
+            },
+            rapotList: formattedRaporData,
+          });
         }
-    });
+      });
+    }
+  });
 });
 
-router.get('/imunisasiObatC', (req, res) => {
-    res.render('imunisasiObatC');
+router.get("/imunisasiObatC", (req, res) => {
+  res.render("imunisasiObatC");
 });
 
 // routes/pages.js
-router.post('/submitImunisasi', (req, res) => {
-    const { nisn, tanggal_tindakan, kode_obat, reaksi_24_jam, reaksi_72_jam, reaksi_1_minggu, efek_samping } = req.body;
+router.post("/submitImunisasi", (req, res) => {
+  const {
+    nisn,
+    tanggal_tindakan,
+    kode_obat,
+    reaksi_24_jam,
+    reaksi_72_jam,
+    reaksi_1_minggu,
+    efek_samping,
+  } = req.body;
 
-    const nama_kegiatan = 'Imunisasi Obat C';
+  const nama_kegiatan = "Imunisasi Obat C";
 
-    // Check if the provided nisn exists in the anak table
-    const checkNisnQuery = 'SELECT COUNT(*) AS count FROM anak WHERE nisn = ?';
+  // Check if the provided nisn exists in the anak table
+  const checkNisnQuery = "SELECT COUNT(*) AS count FROM anak WHERE nisn = ?";
 
-    db.query(checkNisnQuery, [nisn], (checkError, checkResult) => {
-        if (checkError) {
-            console.log(checkError);
-            // Handle the error, e.g., show an error page
-            res.render('imunisasiObatC', { message: 'Error checking nisn.' });
-        } else {
-            const count = checkResult[0].count;
-            if (count === 0) {
-                // nisn not found, handle accordingly (e.g., show a message)
-                res.render('imunisasiObatC', { message: 'NISN yang anda masukan salah' });
+  db.query(checkNisnQuery, [nisn], (checkError, checkResult) => {
+    if (checkError) {
+      console.log(checkError);
+      // Handle the error, e.g., show an error page
+      res.render("imunisasiObatC", { message: "Error checking nisn." });
+    } else {
+      const count = checkResult[0].count;
+      if (count === 0) {
+        // nisn not found, handle accordingly (e.g., show a message)
+        res.render("imunisasiObatC", {
+          message: "NISN yang anda masukan salah",
+        });
+      } else {
+        // Construct the INSERT query
+        const insertQuery =
+          "INSERT INTO rapot (anak_id, tanggal_tindakan, nama_kegiatan, kode_obat, reaksi_24_jam, reaksi_72_jam, reaksi_1_minggu, efek_samping) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+        // Execute the INSERT query
+        db.query(
+          insertQuery,
+          [
+            nisn,
+            tanggal_tindakan,
+            nama_kegiatan,
+            kode_obat,
+            reaksi_24_jam,
+            reaksi_72_jam,
+            reaksi_1_minggu,
+            efek_samping,
+          ],
+          (error, result) => {
+            if (error) {
+              console.log(error);
+              // Handle the error, e.g., show an error page
+              res.render("imunisasiObatC", {
+                message: "Error submitting data.",
+              });
             } else {
-                // Construct the INSERT query
-                const insertQuery = 'INSERT INTO rapot (anak_id, tanggal_tindakan, nama_kegiatan, kode_obat, reaksi_24_jam, reaksi_72_jam, reaksi_1_minggu, efek_samping) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
-                
-                // Execute the INSERT query
-                db.query(insertQuery, [nisn, tanggal_tindakan, nama_kegiatan, kode_obat, reaksi_24_jam, reaksi_72_jam, reaksi_1_minggu, efek_samping], (error, result) => {
-                    if (error) {
-                        console.log(error);
-                        // Handle the error, e.g., show an error page
-                        res.render('imunisasiObatC', { message: 'Error submitting data.' });
-                    } else {
-                        // Successful insertion
-                        res.render('imunisasiObatC', { message: `${nisn} berhasil dimasukan` }); // Redirect back to the form page
-                    }
-                });
+              // Successful insertion
+              res.render("imunisasiObatC", {
+                message: `${nisn} berhasil dimasukan`,
+              }); // Redirect back to the form page
             }
-        }
-    });
+          }
+        );
+      }
+    }
+  });
 });
-
-
-
 
 module.exports = router;
